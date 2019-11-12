b299e31a96903ac5ce9a7c467614d3a644535ed2
Mc-fly82 <flavius_marc@yahoo.fr>
2019-11-12 16:41:30 +0100

    add

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,71 +1,79 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import Clipboard from 'clipboard';
-import moment from 'moment';
+import { Meteor } from 'meteor/meteor'
+import React      from 'react'
+import Clipboard  from 'clipboard'
+import moment     from 'moment'
 
 export default class LinksListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      justCopied: false
-    };
-  }
-  componentDidMount() {
-    this.clipboard = new Clipboard(this.refs.copy);
+	constructor(props) {
+		super(props)
+		this.state = {
+			justCopied: false,
+		}
+	}
 
-    this.clipboard.on('success', () => {
-      this.setState({ justCopied: true });
-      setTimeout(() => this.setState({ justCopied: false }), 1000);
-    }).on('error', () => {
-      alert('Impossible de copier. Veuillez copier manuellement le lien.');
-    })
-  }
-  componentWillUnmount() {
-    this.clipboard.destroy();
-  }
-  renderStats() {
-    const visitMessage = this.props.visitedCount === 1 ? 'voir' : 'vu(s)';
-    let visitedMessage = null;
+	componentDidMount() {
+		this.clipboard = new Clipboard(this.refs.copy)
 
-    if (typeof this.props.lastVisitedAt === 'number') {
-      visitedMessage = `(vu ${ moment(this.props.lastVisitedAt).fromNow() })`;
-    }
+		this.clipboard.on('success', () => {
+			this.setState({justCopied: true})
+			setTimeout(() => this.setState({justCopied: false}), 1000)
+		}).on('error', () => {
+			alert('Impossible de copier. Veuillez copier manuellement le lien.')
+		})
+	}
 
-    return <p className="item__message">{this.props.visitedCount} {visitMessage} {visitedMessage}</p>;
-  }
-  render() {
-    return (
-      <div className="item">
-        <h2>{this.props.url}</h2>
-        <p className="item__message">{this.props.shortUrl}</p>
-        {this.renderStats()}
-        <a className="button button--pill button--link" href={this.props.shortUrl} target="_blank">
-          Visiter
-        </a>
-        <button className="button button--pill" ref="copy" data-clipboard-text={this.props.shortUrl}>
-          {this.state.justCopied ? 'Ok' : 'Copier'}
-        </button>
-        <button className="button button--pill" onClick={() => {
-          Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
-        }}>
-          {this.props.visible ? 'Archiver' : 'Déarchiver'}
-        </button>
-        <button className="button button--pill" onClick={() => {
-          Meteor.call('links.delete', this.props._id);
-        }}>
-          Supprimer
-        </button>
-      </div>
-    );
-  }
+	componentWillUnmount() {
+		this.clipboard.destroy()
+	}
+
+	renderStats() {
+		const visitMessage = this.props.visitedCount === 1
+			? 'voir'
+			: 'vu(s)'
+		let visitedMessage = null
+
+		if (typeof this.props.lastVisitedAt === 'number') {
+			visitedMessage = `(vu ${moment(this.props.lastVisitedAt).fromNow()})`
+		}
+
+		return <p className="item__message">{this.props.visitedCount} {visitMessage} {visitedMessage}</p>
+	}
+
+	render() {
+		return (<div className="item">
+			<h2>{this.props.url}</h2>
+			<p className="item__message">{this.props.shortUrl}</p>
+			{this.renderStats()}
+			<a className="button button--pill button--link" href={this.props.shortUrl} target="_blank">
+				Visiter
+			</a>
+			<button className="button button--pill" ref="copy" data-clipboard-text={this.props.shortUrl}>
+				{this.state.justCopied
+					? 'Ok'
+					: 'Copier'}
+			</button>
+			<button className="button button--pill" onClick={() => {
+				Meteor.call('links.setVisibility', this.props._id, !this.props.visible)
+			}}>
+				{this.props.visible
+					? 'Archiver'
+					: 'Déarchiver'}
+			</button>
+			<button className="button button--pill" onClick={() => {
+				Meteor.call('links.delete', this.props._id)
+			}}>
+				Supprimer
+			</button>
+		</div>)
+	}
 };
 
 LinksListItem.propTypes = {
-  _id: React.PropTypes.string.isRequired,
-  url: React.PropTypes.string.isRequired,
-  userId: React.PropTypes.string.isRequired,
-  visible: React.PropTypes.bool.isRequired,
-  shortUrl: React.PropTypes.string.isRequired,
-  visitedCount: React.PropTypes.number.isRequired,
-  lastVisitedAt: React.PropTypes.number
-};
+	_id: React.PropTypes.string.isRequired,
+	url: React.PropTypes.string.isRequired,
+	userId: React.PropTypes.string.isRequired,
+	visible: React.PropTypes.bool.isRequired,
+	shortUrl: React.PropTypes.string.isRequired,
+	visitedCount: React.PropTypes.number.isRequired,
+	lastVisitedAt: React.PropTypes.number,
+}
